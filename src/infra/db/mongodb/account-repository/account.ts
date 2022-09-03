dabf1e0339ce8eb3ae56611bbcab87bfa7192bff
pedrohenjs <pedrojs.contato@gmail.com>
2022-09-03 10:46:18 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,16 +8,7 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = await accountCollection.findOne({ _id: result.insertedId })
-    console.log(account)
-    const formatedAccount: AccountModel = {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      id: `ObjectId("${account._id}")`,
-      email: account.email,
-      name: account.name,
-      password: account.password
 
-    }
-    console.log(formatedAccount)
-    return formatedAccount
+    return MongoHelper.accountMapper(account)
   }
 }
