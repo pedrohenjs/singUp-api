646ab43f8041dd595a1ed63291dd6eeb2218e623
pedrohenjs <pedrojs.contato@gmail.com>
2022-09-03 21:33:26 -0300

    feat: ensure request return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
+import { contentType } from '../middlewares/content_type'
 import { cors } from '../middlewares/cors'
 import { bodyParser } from './../middlewares/body-parser'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
